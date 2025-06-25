bb2957e077e9df17c4039ce3001ad453f71e49e8
Nicolas Paiva <nicolaspaiva@outlook.com>
2025-06-25 21:56:58 +0100

    finished initial design of the navbar and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Navbar from '@/components/ui/Navbar';
+import {Button} from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <>
             <Navbar/>
+            <div className="w-[90vw] lg:max-w-7xl mx-auto text-center py-20">
+                <h1 className="text-4xl font-bold">Finance Vault</h1>
+                <p className="mt-4 text-lg text-muted-foreground">
+                    Manage your expenses, track your income, and stay in control.
+                </p>
+                <div className="mt-6 flex flex-col justify-center items-center gap-4">
+                    <div className="relative w-[350px] h-[350px] md:w-[550px] md:h-[550px]">
+                        <Image src="/images/vault.jpg"
+                               className="object-cover rounded"
+                               fill
+                               alt="Banking">
+                        </Image>
+                    </div>
+                    <div className="flex flex-row justify-between mt-6 w-[80%] md:w-60">
+                        <Button className="md:mr-4">Get Started</Button>
+                        <Button variant="outline">Learn More</Button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,21 +1,29 @@
+"use client"
+
 import Image from 'next/image';
 import {Button} from '@/components/ui/button';
+import {ThemeToggle} from '@/components/ui/ThemeToggle';
+import {useTheme} from 'next-themes';
 
 export default function Navbar() {
-    return <nav className="h-[75px] w-[90vw] lg:max-w-7xl mx-auto flex justify-between items-center max-width">
-        <Image src="/images/logo-dark.png"
+
+    const {theme} = useTheme();
+
+    return <nav className="h-[75px] w-[90vw] lg:max-w-7xl mx-auto flex justify-between items-center">
+        <Image src={theme === "dark" ? "/images/logo-dark.png" : "/images/logo-light.png"}
                width={75}
                height={75}
                alt="Finance Vault"
         />
 
-        <div className="flex justify-between w-[250px]">
+        <div className="hidden md:flex md:justify-between">
             <Button variant="link" className="text-foreground">About Us</Button>
             <Button variant="link" className="text-foreground">Contact Us</Button>
         </div>
 
-        <div className="flex">
-            <Button className="mr-4">Sign up</Button>
+        <div className="flex justify-between sm:w-[45%] md:w-[30%] lg:w-[25%]">
+            <ThemeToggle/>
+            <Button className="mr-2 ml-2">Sign up</Button>
             <Button variant="secondary">Log in</Button>
         </div>
     </nav>;
