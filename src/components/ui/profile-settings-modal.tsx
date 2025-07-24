9b05c48e30ef68d7964829d5f1c1c24640653511
Nicolas Paiva <nicolaspaiva@outlook.com>
2025-07-24 18:38:20 +0100

    improved UX in the profile page

--- src/components/ui/profile-settings-modal.tsx
+++ src/components/ui/profile-settings-modal.tsx
@@ -3,8 +3,9 @@ import {Label} from '@/components/ui/label';
 import {FiEdit2} from 'react-icons/fi';
 import {Input} from '@/components/ui/input';
 import {Button} from '@/components/ui/button';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {EmailChangeRequest, NameChangeRequest, PasswordChangeRequest} from '@/lib/types/profile';
+import {isEmailValid} from '@/lib/utils/utils';
 
 type ProfileSettingsModalProps = {
     type: string,
@@ -27,19 +28,57 @@ export default function ProfileSettingsModal({type, onSubmit}: ProfileSettingsMo
 
     const [buttonInactive, setButtonInactive] = useState(true);
 
+    // Error
     const [emailError, setEmailError] = useState(false);
+    const [confirmationEmailError, setConfirmationEmailError] = useState(false);
 
-    // Changes the name state and enables the button
-    // if there is also a last name
-    function nameChange(value: string): void {
-        setNewName(value);
-
-        if (newLastName.length !== 0) {
+    /**
+     * Enables the submit button only if name and last name
+     * have a length superior greater or equal to 1
+     */
+    useEffect(() => {
+        if (newName.length > 0 && newLastName.length > 0) {
             setButtonInactive(false);
-        }
-
-        if (newLastName.length === 0) {
+        } else {
             setButtonInactive(true);
+        }
+    }, [newName, newLastName]);
+
+    /**
+     * Enables the submit button only if both
+     * email match and have a length greater
+     * or equal to 1
+     */
+    useEffect(() => {
+        if (newEmail === newEmailConfirmation && newEmail.length > 0) {
+            setButtonInactive(false);
+        } else {
+            setButtonInactive(true);
+        }
+    }, [newEmail, newEmailConfirmation]);
+
+
+    /**
+     * Checks whether the provided email has a valid format
+     * setting/removing the error message
+     */
+    function emailValidation(email: string): void {
+        setEmail(email);
+
+        if (!isEmailValid(email)) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    }
+
+    function confirmationEmailValidation(email: string): void {
+        setConfirmationEmail(email);
+
+        if (email !== newEmail) {
+            setConfirmationEmailError(true);
+        } else {
+            setConfirmationEmailError(false);
         }
     }
 
@@ -73,11 +112,30 @@ export default function ProfileSettingsModal({type, onSubmit}: ProfileSettingsMo
                 onSubmit(passwordRequest);
                 break;
         }
+
+        setButtonInactive(true);
+    }
+
+
+    /**
+     * Resets the state back to default
+     */
+    function resetModal(): void {
+        setNewName('');
+        setNewLastName('');
+        setEmail('');
+        setConfirmationEmail('');
+        setOldPassword('');
+        setNewPassword('');
+        setNewConfirmationPassword('');
+        setButtonInactive(true);
+        setEmailError(false);
+        setConfirmationEmailError(false);
     }
 
 
     return (
-        <Dialog>
+        <Dialog onOpenChange={resetModal}>
             <div className="flex">
                 <DialogTrigger>
                     <FiEdit2 className="ml-4 hover:text-violet-600"/>
@@ -95,19 +153,24 @@ export default function ProfileSettingsModal({type, onSubmit}: ProfileSettingsMo
                     </>
                 }
 
-                <Label htmlFor={`new-${type}`}>{type === 'name' ? 'Name' : 'New ' + type}</Label>
-                <Input type={type} id={`new-${type}`} onChange={(e) => {
-                    switch (type) {
-                        case 'name':
-                            nameChange(e.target.value);
-                            break;
-                        case 'email':
-                            setEmail(e.target.value);
-                            break;
-                        case 'password':
-                            setNewPassword(e.target.value);
-                    }
-                }}/>
+                <div>
+                    <Label htmlFor={`new-${type}`} className="mb-2">{type === 'name' ? 'Name' : 'New ' + type}</Label>
+                    <Input type={type}
+                           className="mb-0"
+                           id={`new-${type}`} onChange={(e) => {
+                        switch (type) {
+                            case 'name':
+                                setNewName(e.target.value);
+                                break;
+                            case 'email':
+                                emailValidation(e.target.value);
+                                break;
+                            case 'password':
+                                setNewPassword(e.target.value);
+                        }
+                    }}/>
+                    {emailError && <p className="text-xs text-destructive">Please provide a valid email</p>}
+                </div>
 
                 {/* Last name input */}
                 {type === 'name' &&
@@ -119,19 +182,21 @@ export default function ProfileSettingsModal({type, onSubmit}: ProfileSettingsMo
                 }
 
                 {/* Confirmation password/email */}
+
                 {type === 'name' ? '' :
-                    <>
-                        <Label htmlFor={`confirm-new-${type}`}>Confirm new {type}</Label>
+                    <div>
+                        <Label htmlFor={`confirm-new-${type}`} className="mb-2">Confirm new {type}</Label>
                         <Input id={`confirm-new-${type}`} type={type} onChange={(e) => {
                             switch (type) {
                                 case 'email':
-                                    setConfirmationEmail(e.target.value);
+                                    confirmationEmailValidation(e.target.value)
                                     break;
                                 case 'password':
                                     setNewConfirmationPassword(e.target.value);
                             }
                         }}/>
-                    </>
+                        {confirmationEmailError && <p className="text-xs text-destructive">Both email must match</p>}
+                    </div>
                 }
                 <DialogClose asChild>
                     <Button disabled={buttonInactive} type="submit" className="mt-2" onClick={changeData}>Save</Button>
